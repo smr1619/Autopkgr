ecaf1ed018e1275b2d1e73b2c955429cd1608082
smr1619 <43095131+smr1619@users.noreply.github.com>
2023-08-01 10:16:02 -0700

    Update All_Update.sh

--- All_Update.sh
+++ All_Update.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Set the download directory
-download_dir="/users/desktop/Autopkg_Files"
-
 # Set the list of AutoPkg recipes to run
 recipes=(
-    "com.github.smr1619.Rectangle.sr.jamf.recipe"
-    "com.github.smr1619.AdobeCC.sr.jamf.recipe"
-    "com.github.smr1619.AdobeAcrobat.sr.jamf.recipe"
-    "com.github.smr1619.AdobeReader.sr.jamf.recipe"
-    "com.github.smr1619.Chrome.sr.jamf.recipe"
-    "com.github.smr1619.Edge.sr.jamf.recipe"
-    "com.github.smr1619.firefox.sr.jamf.recipe"
-    "com.github.smr1619.zoom.sr.jamf.recipe"
+    "com.github.data-JAR-recipes.pkg.Rectangle"
+    "com.github.beatlemike.jamf.AdobeCCDesktop"
+    "com.github.beatlemike.jamf.AdobeAcrobatDCUpdate"
+    "com.github.beatlemike.jamf.AdobeAcrobatReaderDCUpdate"
+    "com.github.autopkg.pkg.googlechrome"
+    "com.github.rtrouton.pkg.microsoftedge"
+    "com.github.rtrouton.pkg.Firefox"
+    "com.github.homebysix.pkg.Zoom"
 )
 
 # Check if AutoPkg is installed
@@ -21,13 +18,7 @@ if ! command -v autopkg &> /dev/null; then
     exit 1
 fi
 
-# Update the trust information for all recipes in AutoPkg
-autopkg update-trust-info
-
 # Run each AutoPkg recipe to download the packages to the download directory
 for recipe in "${recipes[@]}"; do
     autopkg run "$recipe" --post "io.github.hjuutilainen.VirusTotalAnalyzer/VirusTotalAnalyzer"
 done
-
-# Move the downloaded packages to the download directory
-mv /Library/AutoPkg/Cache/*/*.pkg "$download_dir"
